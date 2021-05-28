1d2d5cae306f877d1e47611ee7a5ebf5b6e703c3
MohamedTarek251999 <52556495+Muhammed-El-Sayed@users.noreply.github.com>
2021-05-28 21:50:06 +0200

    Update GPS.h

--- GPS.h
+++ GPS.h
@@ -7,17 +7,15 @@
 #include <math.h>
 
 
-/*Data Types*/
- extern uint8 buffer[];
- extern uint8 lat[];
- extern uint8 lon[];
-
 
 /*Functions*/
-float64 Return_Latitude_In_Degrees (void);
-float64 Return_Longitude_In_Degrees (void);
+uint8 Return_Latitude_Direction(uint8 * buffer);
+uint8 Return_Longitude_Direction(uint8 * buffer);
+float64 Return_Latitude_In_Degrees (uint8 * buffer);
+float64 Return_Longitude_In_Degrees (uint8 * buffer);
 float64 toRadians(const float64 degree);
 float64 calculate_Distance_between_2_Coordinates(float64 lat1, float64 long1,float64 lat2,float64 long2);
+
 
 
 #endif
