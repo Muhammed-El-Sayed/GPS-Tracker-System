7149cca392c2ad2b5fd51ca81a15b2abfdf119c8
MohamedTarek251999 <52556495+Muhammed-El-Sayed@users.noreply.github.com>
2021-05-29 00:22:01 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,19 @@
 
+#include"uart.h"
+
+int main()
+{
+  uint8 string []= {'U','A','R','T','#','\0'};
+  UART_Config Configuration1 ={UART_1_TX_PB1,DATA_LENGTH_8_BITS,ONE_STOP_BIT};
+  
+  UART_Init(&Configuration1);
+  
+  while(1)
+
+   //UART_Send_Byte(0xA7, UART_1_TX_PB1);
+   UART_sendString(string, UART_1_TX_PB1);
+
+   //UART_Send_Byte(0xEE, UART_1_TX_PB1);
+   
+  return 0;
+}
