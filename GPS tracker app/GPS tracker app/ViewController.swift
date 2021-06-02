9a9f0b37868957b139e614a39a231ce2855fd914
MohamedKhaled563 <mohamedkhlidmkm0@gmail.com>
2021-06-02 21:12:54 +0200

    -

--- GPS tracker app/GPS tracker app/ViewController.swift
+++ GPS tracker app/GPS tracker app/ViewController.swift
@@ -5,20 +5,13 @@
 //  Created by Mohamed Khalid on 02/06/2021.
 //
 
-import Cocoa
+import UIKit
 
-class ViewController: NSViewController {
+class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
-    }
-
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
-        }
     }
 
 
