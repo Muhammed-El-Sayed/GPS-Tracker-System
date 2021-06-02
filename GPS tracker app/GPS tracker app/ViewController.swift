071c8245e99416f5171fb92f2fe55675c19b5e0c
MohamedKhaled563 <mohamedkhlidmkm0@gmail.com>
2021-06-02 21:08:10 +0200

    iOS application startup project changed

---
+++ GPS tracker app/GPS tracker app/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  GPS tracker app
+//
+//  Created by Mohamed Khalid on 02/06/2021.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
