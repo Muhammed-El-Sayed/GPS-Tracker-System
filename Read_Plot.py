639e12d5350e0d8131660c4ac44ab18031cc9f2a
Mohamed AbdelAzeem <42822972+MohamedAbdelAzeem@users.noreply.github.com>
2021-06-06 21:32:14 +0200

    Python updated - readings

--- Read_Plot.py
+++ Read_Plot.py
@@ -6,9 +6,10 @@ longitude_list_test = []
 latitude_list_test = []
 file = open("hello.txt","r")
 for line in file :
-    fiels = line.split(" ")
+    fiels = line.split(",")    #15.46512,13.46162 ##
     longitude_list_test.append(fiels[0])
-    latitude_list_test.append(fiels[1])
+    x = fiels[1].split(" ")
+    latitude_list_test.append(x[0])
 
 print(longitude_list_test)
 print(latitude_list_test)
@@ -22,19 +23,18 @@ for item in latitude_list_test:
     latitude_list_test_float.append(float(item))
 
     
-longitude_list = [ 31.28448, 31.28274, 31.28159, 31.28010, 31.27922, 31.27833, 31.27629, 31.27406 ]
-latitude_list = [ 30.07327, 30.07140, 30.07024, 30.06875, 30.06791, 30.06698, 30.06529,30.06450  ]
 
-gmap3 = gmplot.GoogleMapPlotter(30.07327,
-                                31.28448, 15)
-  
+gmap3 = gmplot.GoogleMapPlotter(longitude_list_test[0],  latitude_list_test[0], 25)
+
+#gmap3 = gmplot.GoogleMapPlotter(30.07327,
+ #                               31.28448, 15)
 # scatter method of map object 
 # scatter points on the google map
-gmap3.scatter( latitude_list_test, longitude_list_test , '#FF0000',
-                              size = 40, marker = False )
+gmap3.scatter( longitude_list_test_float, latitude_list_test_float, '#FF0000',
+                              size = 2, marker = False )
   
 # Plot method Draw a line in
 # between given coordinates
-gmap3.plot(latitude_list_test_float, longitude_list_test_float, 
+gmap3.plot(longitude_list_test_float, latitude_list_test_float,
            'cornflowerblue', edge_width = 2.5)
-gmap3.draw( "C:\\Users\\ZAMALA\\Desktop\\map13.html" )
+gmap3.draw( "C:\\Users\\g.version\\Desktop\\map13.html" )
